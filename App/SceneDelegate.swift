da3f2266f46c0ede62229c7d3eb4437b01ef4b98
신동규 <donggyu9410@gmail.com>
2024-05-17 22:30:25 +0900

    Presentation

--- App/SceneDelegate.swift
+++ App/SceneDelegate.swift
@@ -6,17 +6,18 @@
 //
 
 import UIKit
+import Presentation
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = .init(windowScene: scene)
+        window?.rootViewController = Presentation.SearchUserViewController()
+        window?.makeKeyAndVisible()
+        return
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ Presentation/SearchUserViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchUserViewController.swift
+//  Presentation
+//
+//  Created by 신동규 on 5/17/24.
+//
+
+import UIKit
+
+public final class SearchUserViewController: UIViewController {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+    }
+    
+    private func configUI() {
+        view.backgroundColor = .purple
+    }
+}
