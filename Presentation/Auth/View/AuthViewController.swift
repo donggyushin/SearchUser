8edea05ecdd930232003c574c0715b6951f56bad
신동규 <donggyu9410@gmail.com>
2024-05-18 13:14:02 +0900

    AuthViewController UI

--- Presentation/Auth/View/AuthViewController.swift
+++ Presentation/Auth/View/AuthViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 public final class AuthViewController: UIViewController {
+    
+    private lazy var githubLoginButton = UIButton(configuration: .borderedTinted(), primaryAction: UIAction(handler: { _ in
+        print("tap button")
+    }))
+        .then { button in
+            button.setTitle("Sign in with Github", for: .normal)
+        }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         configUI()
     }
     
     private func configUI() {
-        view.backgroundColor = .blue
+        view.addSubview(githubLoginButton)
+        githubLoginButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
+}
+
+#Preview {
+    let vc = AuthViewController()
+    return vc
 }
