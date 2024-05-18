5563b8f603614acc561e4b9bcfdc3a70212eeb92
신동규 <donggyu9410@gmail.com>
2024-05-18 14:51:28 +0900

    presentation: model

---
+++ Presentation/User/Model/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  Presentation
+//
+//  Created by 신동규 on 5/18/24.
+//
+
+import Foundation
+import Domain
+
+struct User {
+    let id: String
+    let login: String
+    let avatarUrl: String?
+    let url: String
+    
+    init(domain: Domain.User) {
+        id = domain.id
+        login = domain.login
+        avatarUrl = domain.avatarUrl
+        url = domain.url
+    }
+    
+    var domain: Domain.User {
+        .init(id: id,
+              login: login,
+              avatarUrl: avatarUrl,
+              url: url)
+    }
+}
