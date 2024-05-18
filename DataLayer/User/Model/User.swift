a999cc7036428f63a191b87f6f1ab856f507e182
신동규 <donggyu9410@gmail.com>
2024-05-18 14:36:01 +0900

    data: Model

---
+++ DataLayer/User/Model/User.swift
@@ -0,0 +1,37 @@
+//
+//  User.swift
+//  DataLayer
+//
+//  Created by 신동규 on 5/18/24.
+//
+
+import Foundation
+import Domain
+
+struct User: Codable {
+    let id: String
+    let login: String
+    let avatarUrl: String?
+    let url: String
+    
+    init(domain: Domain.User) {
+        id = domain.id
+        login = domain.login
+        avatarUrl = domain.avatarUrl
+        url = domain.url
+    }
+    
+    var domain: Domain.User {
+        .init(id: id,
+              login: login,
+              avatarUrl: avatarUrl,
+              url: url)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case login
+        case avatarUrl = "avatar_url"
+        case url = "html_url"
+    }
+}
