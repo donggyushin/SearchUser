79595837a2d2694ba4806f6152008b1a57f21bf3
신동규 <donggyu9410@gmail.com>
2024-05-18 17:32:02 +0900

    change mock repository

--- Presentation/MockData/User/Repository/UserRepositoryImpl.swift
+++ Presentation/MockData/User/Repository/UserRepositoryImpl.swift
@@ -10,14 +10,16 @@ import Domain
 
 final class UserRepositoryImpl: UserRepository {
     func get(query: String, page: Int, perPage: Int) async throws -> ([Domain.User], totalCount: Int) {
-        let users: [Domain.User] = [
+        var users: [Domain.User] = [
             USER_1,
             USER_2,
             USER_3,
             USER_4
         ]
         
-        let totalCount = 12
+        users = users.filter({ $0.login.contains(query) })
+        
+        let totalCount = users.count
         
         return (users, totalCount)
     }
